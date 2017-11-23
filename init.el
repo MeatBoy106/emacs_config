49fb6051494cd49a5ac4b763664c781f23c08930
Gabriel Busnot <gabibusnot@gmail.com>
2017-11-23 22:42:54 +0100

    Native emacs config file

---
+++ init.el
@@ -0,0 +1,14 @@
+(setq inhibit-startup-message t)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
